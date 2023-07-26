eabb270cb1f410e6236ee361934a025712eaf8d7
summermong <2514o_o@naver.com>
2023-07-27 02:44:43 +0900

    :sparkles: useParams, useSearchParams, useNavigate 적용하기

--- src/pages/Diary.js
+++ src/pages/Diary.js
@@ -1,6 +1,12 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 
 const Diary = () => {
+  // React에서 제공하는 Hooks는 아님
+  // 하지만 별도 라이브러리가 자신의 라이브러리 기능을 편하게 하려고 만든 게 사용자 정의 훅(커스텀 훅)
+  const { id } = useParams();
+  console.log(id);
+
   return (
     <div className="Diary">
       <h2>Diary</h2>

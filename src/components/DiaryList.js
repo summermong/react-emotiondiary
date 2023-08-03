9383537b15985160021589412ea413091c88f4ec
summermong <2514o_o@naver.com>
2023-08-03 12:54:35 +0900

    :sparkles: feat: 최신순/오래된순으로 정렬

--- src/components/DiaryList.js
+++ src/components/DiaryList.js
@@ -1,9 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+const sortOptionList = [
+  { value: 'latest', name: '최신순' },
+  { value: 'oldest', name: '오래된 순' },
+];
+
+const ControlMenu = ({ value, onChange, optionList }) => {
+  return (
+    <select value={value} onChange={(e) => onChange(e.target.value)}>
+      {optionList.map((it, idx) => (
+        <option key={idx} value={it.value}>
+          {it.name}
+        </option>
+      ))}
+    </select>
+  );
+};
 
 const DiaryList = ({ diaryList }) => {
+  const [sortType, setSortType] = useState('lastest');
+  const getProcessedDiaryList = () => {
+    const compare = (a, b) => {
+      if (sortType === 'lastest') {
+        return parseInt(b.date) - parseInt(a.date);
+      } else {
+        return parseInt(a.date) - parseInt(b.date);
+      }
+    };
+
+    // sort 하면 원본 배열이 변경되니 깊은 복사
+    const copyList = JSON.parse(JSON.stringify(diaryList));
+    const sortedList = copyList.sort(compare);
+    return sortedList;
+  };
+
   return (
     <div>
-      {diaryList.map((it) => (
+      <ControlMenu
+        value={sortType}
+        onChange={setSortType}
+        optionList={sortOptionList}
+      />
+      {getProcessedDiaryList().map((it) => (
         <div key={it.id}>{it.content}</div>
       ))}
     </div>

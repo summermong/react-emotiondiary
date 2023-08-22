90aca4dc763eb2497b271e9398532c5eb0acd860
summermong <2514o_o@naver.com>
2023-08-22 14:18:51 +0900

    :sparkles: feat: 날짜 input 구현

--- src/pages/New.js
+++ src/pages/New.js
@@ -1,10 +1,36 @@
-import React from "react";
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Myheader from '../components/Myheader';
+import Mybtn from './../components/Mybtn';
+
+//ISO 형식의 문자열 반환
+const getStringDate = (date) => {
+  return date.toISOString().slice(0, 10);
+};
 
 const New = () => {
+  // 오늘의 날짜를 ISO 형식으로 바꾼 문자열이 날짜의 초기값이 되도록
+  const [date, setDate] = useState(getStringDate(new Date()));
+  const navigate = useNavigate();
   return (
-    <div className="New">
-      <h2>New</h2>
-      <p>이 곳은 추가하는 곳입니다.</p>
+    <div>
+      <Myheader
+        headText={'새 일기 쓰기'}
+        leftChild={<Mybtn onClick={() => navigate(-1)} text={'< 뒤로 가기'} />}
+      />
+      <div>
+        <section>
+          <h4>오늘의 날짜</h4>
+          <div className="input-box">
+            <input
+              className="input-date"
+              value={date}
+              onChange={(e) => setDate(e.target.value)}
+              type="date"
+            />
+          </div>
+        </section>
+      </div>
     </div>
   );
 };

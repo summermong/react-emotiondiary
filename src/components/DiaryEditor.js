17a862198b98ab32735db02dfc772053fd028f30
summermong <2514o_o@naver.com>
2023-09-05 17:22:02 +0900

    :lipstick: feat: 일기 쓰기의 오늘의 감정 기능 구현

--- src/components/DiaryEditor.js
+++ src/components/DiaryEditor.js
@@ -2,6 +2,35 @@ import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Myheader from './Myheader';
 import Mybtn from './Mybtn';
+import EmotionItem from './EmotionItem';
+
+const emotionList = [
+  {
+    emotion_id: 1,
+    emotion_img: process.env.PUBLIC_URL + `/assets/emotion1.png`,
+    emotion_descript: '완전 좋음',
+  },
+  {
+    emotion_id: 2,
+    emotion_img: process.env.PUBLIC_URL + `/assets/emotion2.png`,
+    emotion_descript: '좋음',
+  },
+  {
+    emotion_id: 3,
+    emotion_img: process.env.PUBLIC_URL + `/assets/emotion3.png`,
+    emotion_descript: '그럭저럭',
+  },
+  {
+    emotion_id: 4,
+    emotion_img: process.env.PUBLIC_URL + `/assets/emotion4.png`,
+    emotion_descript: '나쁨',
+  },
+  {
+    emotion_id: 5,
+    emotion_img: process.env.PUBLIC_URL + `/assets/emotion5.png`,
+    emotion_descript: '완전 나쁨',
+  },
+];
 
 //ISO 형식의 문자열 반환
 const getStringDate = (date) => {
@@ -9,6 +38,12 @@ const getStringDate = (date) => {
 };
 
 const DiaryEditor = () => {
+  const [emotion, setEmotion] = useState(3);
+
+  const handleClickEmote = (emotion) => {
+    setEmotion(emotion);
+  };
+
   // 오늘의 날짜를 ISO 형식으로 바꾼 문자열이 날짜의 초기값이 되도록
   const [date, setDate] = useState(getStringDate(new Date()));
   const navigate = useNavigate();
@@ -28,6 +63,19 @@ const DiaryEditor = () => {
               onChange={(e) => setDate(e.target.value)}
               type="date"
             />
+          </div>
+        </section>
+        <section>
+          <h4>오늘의 감정</h4>
+          <div className="input_box emotion_list_wrapper">
+            {emotionList.map((it) => (
+              <EmotionItem
+                key={it.emotion_id}
+                {...it}
+                onClick={handleClickEmote}
+                isSelected={it.emotion_id === emotion}
+              />
+            ))}
           </div>
         </section>
       </div>

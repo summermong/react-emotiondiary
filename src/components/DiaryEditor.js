9416943647d278d84fe472077da2304aa95d7566
summermong <2514o_o@naver.com>
2023-09-05 17:37:52 +0900

    :lipstick: feat: 일기 쓰기의 오늘의 일기 기능 구현

--- src/components/DiaryEditor.js
+++ src/components/DiaryEditor.js
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
+import React, { useContext, useRef, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { DiaryDispatchContext } from './../App.js';
 import Myheader from './Myheader';
 import Mybtn from './Mybtn';
 import EmotionItem from './EmotionItem';
@@ -38,6 +39,20 @@ const getStringDate = (date) => {
 };
 
 const DiaryEditor = () => {
+  const [content, setContent] = useState('');
+  const contentRef = useRef();
+
+  const { onCreate } = useContext(DiaryDispatchContext);
+
+  const handleSubmit = () => {
+    if (content.length < 1) {
+      contentRef.current.focus();
+      return;
+    }
+    onCreate(date, content, emotion);
+    navigate('/', { replace: true }); // 다시 못 돌아오게
+  };
+
   const [emotion, setEmotion] = useState(3);
 
   const handleClickEmote = (emotion) => {
@@ -47,6 +62,7 @@ const DiaryEditor = () => {
   // 오늘의 날짜를 ISO 형식으로 바꾼 문자열이 날짜의 초기값이 되도록
   const [date, setDate] = useState(getStringDate(new Date()));
   const navigate = useNavigate();
+
   return (
     <div className="DiaryEditor">
       <Myheader
@@ -76,6 +92,27 @@ const DiaryEditor = () => {
                 isSelected={it.emotion_id === emotion}
               />
             ))}
+          </div>
+        </section>
+        <section>
+          <h4>오늘의 일기</h4>
+          <div className="input_box text_wrapper">
+            <textarea
+              placeholder="오늘은 어땠나요?"
+              ref={contentRef}
+              value={content}
+              onChange={(e) => setContent(e.target.value)}
+            />
+          </div>
+        </section>
+        <section>
+          <div className="control_box">
+            <Mybtn text={'취소하기'} onClick={() => navigate(-1)} />
+            <Mybtn
+              text={'작성 완료'}
+              type={'positive'}
+              onClick={handleSubmit}
+            />
           </div>
         </section>
       </div>
